03dd7251c60d27a8a818369bfa98b48f7ba0645c
Alan Peña <lanazirot@gmail.com>
2021-03-05 11:20:38 -0600

    Commit hola

--- FIS/src/mx/com/fissoftware/MainDriver.java
+++ FIS/src/mx/com/fissoftware/MainDriver.java
@@ -9,6 +9,18 @@ package mx.com.fissoftware;
 @HazAlgo(algo="123")
 class A{
 	
+	private int algo;
+	
+	public int getAlgo() {
+		return algo;
+	}
+	public void setAlgo(int algo) {
+		this.algo = algo;
+	}
+	
+	public void printAlgo() {
+		System.out.println("Algo");
+	}
 }
 
 
@@ -17,8 +29,11 @@ public class MainDriver {
 	
 	public static void main(String[] args) {
 		
-		A a = new A();
+		var a = new A();
+		a.printAlgo();
 		
+		var claseMala = new SoyUnaClaseMala();
+		claseMala.metodoQueSiSeVaAProduccionTruenaTodo();
 	}
 
 }
